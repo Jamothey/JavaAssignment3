d317e547055c93838ed7433e68ea7c03e4d510c4
Jamothey <jameseyler2000@gmail.com>
2024-03-26 17:18:37 -0400

    package for linked list in assignment 3

---
+++ assignment3/Node.java
@@ -0,0 +1,24 @@
+package assignment3;
+
+
+public class Node
+{
+    int data;
+    Node next;
+
+    public Node(int d)
+    {
+        this.data = data;
+        this.next = null;
+    }
+
+    public int getData()
+    {
+        return data;
+    }
+
+    public Node getNext()
+    {
+        return next;
+    }
+}
